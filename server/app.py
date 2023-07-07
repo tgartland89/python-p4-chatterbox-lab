8f9058fff344faa4678cb381114c6a662d50a689
tgartland89 <thomas.gartland@gmail.com>
2023-07-06 18:43:06 -0600

    everything works good

--- server/app.py
+++ server/app.py
@@ -14,13 +14,65 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/')
+def index():
+    return 'Welcome to the Chatterbox API!'
 
-@app.route('/messages/<int:id>')
+@app.route('/messages', methods=['GET', 'POST'])
+def messages():
+    if request.method == 'GET':
+        messages = Message.query.order_by('created_at').all()
+
+        response = make_response(
+            jsonify([message.to_dict() for message in messages]),
+            200,
+        )
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(message.to_dict()),
+            201,
+        )
+
+    return response
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+            
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(message.to_dict()),
+            200,
+        )
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify({'deleted': True}),
+            200,
+        )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
